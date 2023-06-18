ebffdf94c673d748735fe406278bb6413cbbbd74
Savren <ptrgls94@gmail.com>
2023-06-18 18:46:08 +0200

    Updated webapp
    Encoded categorical data

--- webapp/src/webapp.py
+++ webapp/src/webapp.py
@@ -5,10 +5,35 @@ import numpy as np
 from datetime import datetime
 from flask import Flask, render_template, request, url_for, redirect
 
-
 model_name = os.environ['MODEL_NAME']
 model = pickle.load(open(model_name, 'rb'))
 app = Flask(__name__)
+
+meal_plan_coded = dict({
+    "Meal plan 1": 1,
+    "Meal plan 2": 2,
+    "Meal plan 3": 3,
+    "Not selected": 0
+})
+
+room_type_coded = dict({
+    "Room type 1": 0,
+    "Room type 2": 1,
+    "Room type 3": 2,
+    "Room type 4": 3,
+    "Room type 5": 4,
+    "Room type 6": 5,
+    "Room type 7": 6
+})
+
+market_seg_coded = dict({
+    "Aviation": 4,
+    "Complementary": 3,
+    "Corporate": 2,
+    "Offline": 0,
+    "Online": 1
+})
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -36,11 +61,9 @@ def index():
         not_cancelled = params.get('not_cancelled')
         avg_room_price = params.get('avg_room_price')
         spec_req = params.get('spec_req')
-        meal_plan = 0
-        room_type = 0
-        market_segment = 0
-
-        print(params)
+        meal_plan = meal_plan_coded.get(params.get('mealplan'))
+        room_type = room_type_coded.get(params.get('room_type'))
+        market_segment = market_seg_coded.get(params.get('mkt_seg'))
 
         in_array = np.array([adults, children, wknd_nights, wk_nights,
                              parking_space, lead_time, arrival_year, arrival_month,
@@ -48,6 +71,4 @@ def index():
                              avg_room_price, spec_req, meal_plan, room_type, market_segment])
         in_array = in_array.reshape(1, -1)
         result = model.predict(in_array)
-        print(result)
     return render_template('index.html', params=params, result=result)
-
